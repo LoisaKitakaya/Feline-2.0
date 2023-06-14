71ad1ba29a0269d824a5d49b45ff2b5013b77c6d
Freedom Loisa <kitakayaloisa@gmail.com>
2023-06-14 05:45:30 +0300

    installed react tooltips depoendancies

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
+import Navbar from "./components/nav/Navbar";
+
 const App = () => {
   return (
     <>
+      <Navbar />
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </>
   );
