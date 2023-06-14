87a16459b269168354af2bf92dc6f2863724c603
Freedom Loisa <kitakayaloisa@gmail.com>
2023-06-14 06:37:47 +0300

    installed focus traps react depoendancies

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,9 @@ const App = () => {
   return (
     <>
       <Navbar />
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <div className="mx-4">
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      </div>
     </>
   );
 };
